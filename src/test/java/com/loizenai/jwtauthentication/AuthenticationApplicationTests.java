1c1dde04e93647df8f74897140a12252a5af69b8
Fatma-ouer <ouerghuifatmazahra@gmail.com>
2021-06-09 22:39:06 +0200

    first commit

---
+++ src/test/java/com/loizenai/jwtauthentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loizenai.jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
